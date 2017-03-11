cac5230bb76058392b800efcd1c14cc0c8e90680
RishadYamnoor <rishad.yamnoor@gmail.com>
2017-03-11 10:29:35 +0530

    Education section appended

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -5,7 +5,7 @@ var bio = {
     "role": "Web Developer, UI Designer",
     "contacts": [
     {
-        "mobile": "966-323-9799",
+        "mobile": "+91 966-323-9799",
         "email": "rishad.yammnoor@gmail.com",
         "twitter": "@EvolofThings",
         "github": "evolofthings",
@@ -16,75 +16,7 @@ var bio = {
     "welcomeMsg": "Bringing futuristic ideas to reality",
     "skills": ["HTML", "CSS", "JavaScript", "Git", "JQuery", "Ajax"],
 };
-
-
-var education = {
-    "schools": [
-    {
-        "name": "Northeastern University",
-        "degree": "Masters",
-        "dates": "June 2015",
-        "location": "Boston, MA, US",
-        "major": ["Drug Regulatory Affairs"],
-    },
-    {
-        "name": "Sir M. Vivesvaraya Institute of Technology",
-        "degree": "Under Graduate",
-        "dates": "June 2013",
-        "location": "Bengaluru, KA, India",
-        "major": ["Biotechnology Engineering"],
-    }
-    ],
-    "onlineCourses": [
-    {
-        "school": "Udacity",
-        "title": "Front-end Web Development Nanodegree",
-        "dates": 2017,
-        "url": "www.udacity.com"
-    },
-    {
-        "school": "Free Code Camp",
-        "title": "MOOC Platform",
-        "dates": 2016,
-        "url": "www.freecodecamp.com"
-    }
-    ]
-}
-
-
-var work = {
-    "jobs": [
-    {
-        "jobPosition": "Web Developer",
-        "employer": "Freelance",
-        "dates": 'in progress',
-        "location": "Remote",
-        "description": "Built websites for clients who hired me through freelance working sites"
-    },
-    {
-        "jobPosition": "Clinical SAS Programmer trainee",
-        "employer": "Rang Technologies",
-        "dates": "Oct 2015 to Feb 2016",
-        "location": "Piscataway, NJ, US",
-        "description": "Creating Tables, Lists, Figures and Graphs while conforming to ADam and SDTM coding standards"
-    }
-    ]
-}
-
-
-var projects = {
-    "projects": [
-    {
-        "title": "Portfolio",
-        "dates": "2017",
-        "description": "Portfolio of projects that I have built as part of Udacity Nanodegree program.",
-        "images": ["images/197x148.gif"]
-    }
-    ]
-}
-
 // For-in loop is not suitable for this bio object
-
 bio.display = function() {
     var formattedName = HTMLheaderName.replace("%data%", bio.name);
     var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
@@ -110,12 +42,81 @@ bio.display = function() {
     $("#footerContacts").append(formattedMobile,formattedEmail,formattedTwitter,formattedGithub, formattedLocation);
     }
 }
-bio.display();
+
+
+var education = {
+    "schools": [
+    {
+        "name": "Northeastern University",
+        "degree": "Masters",
+        "dates": "June 2015",
+        "location": "Boston, MA, US",
+        "major": ["Drug Regulatory Affairs"],
+    },
+    {
+        "name": "Sir M. Vivesvaraya Institute of Technology",
+        "degree": "Under Graduate",
+        "dates": "June 2013",
+        "location": "Bengaluru, KA, India",
+        "major": ["Biotechnology Engineering"],
+    }
+    ],
+    "onlineCourses": [
+    {
+        "school": "Udacity",
+        "title": "Front-end Web Development Nanodegree",
+        "dates": 2017,
+        "url": "https://udacity.com"
+    },
+    {
+        "school": "Free Code Camp",
+        "title": "MOOC Platform",
+        "dates": 2016,
+        "url": "https://freecodecamp.com"
+    }
+    ]
+}
+education.display = function () {
+    for (var school in education.schools) {
+        $("#education").append(HTMLschoolStart);
+        var formattedschoolName = HTMLschoolName.replace("%data%", (education.schools[school].name));
+        var formattedschoolDegree = HTMLschoolDegree.replace("%data%", (education.schools[school].degree));
+        var formattedschoolDates = HTMLschoolDates.replace("%data%", (education.schools[school].dates));
+        var formattedschoolLocation = HTMLschoolLocation.replace("%data%", (education.schools[school].location));
+        var formattedschoolMajor = HTMLschoolMajor.replace("%data%", (education.schools[school].major));
+        $(".education-entry:last").append(formattedschoolName + formattedschoolDegree, formattedschoolDates, formattedschoolLocation, formattedschoolMajor);
+    }
+    $("#education").append(HTMLonlineClasses);  //Placed before the loop otherwise it will be repeated twice.
+    for (var online in education.onlineCourses) {
+        $("#education").append(HTMLschoolStart);
+        var formattedonlineTitle = HTMLonlineTitle.replace("%data%", (education.onlineCourses[online].title));
+        var formattedonlineSchool = HTMLonlineSchool.replace("%data%", (education.onlineCourses[online].school));
+        var formattedonlineDates = HTMLonlineDates.replace("%data%", (education.onlineCourses[online].dates));
+        var formattedonlineURL = HTMLonlineURL.replace("%data%", (education.onlineCourses[online].url));
+        $(".education-entry:last").append(formattedonlineTitle + formattedonlineSchool,formattedonlineDates,formattedonlineURL);
+    }
+}
 
 
 
-
-
+var work = {
+    "jobs": [
+    {
+        "jobPosition": "Web Developer",
+        "employer": "Freelance",
+        "dates": 'in progress',
+        "location": "Remote",
+        "description": "Built websites for clients who hired me through freelance working sites"
+    },
+    {
+        "jobPosition": "Clinical SAS Programmer trainee",
+        "employer": "Rang Technologies",
+        "dates": "Oct 2015 to Feb 2016",
+        "location": "Piscataway, NJ, US",
+        "description": "Creating Tables, Lists, Figures and Graphs while conforming to ADam and SDTM coding standards"
+    }
+    ]
+}
 function displayWork() {
     for (var job in work.jobs) {
         $("#workExperience").append(HTMLworkStart);
@@ -135,9 +136,18 @@ function displayWork() {
         $(".work-entry:last").append(formattedDescription);
     }
 }
-displayWork();
 
 
+var projects = {
+    "projects": [
+    {
+        "title": "Portfolio",
+        "dates": "2017",
+        "description": "Portfolio of projects that I have built as part of Udacity Nanodegree program.",
+        "images": ["images/197x148.gif"]
+    }
+    ]
+}
 projects.display = function () {
     for (var project in projects.projects) {
         $("#projects").append(HTMLprojectStart);
@@ -151,7 +161,7 @@ projects.display = function () {
         var formattedDescription = HTMLprojectDescription.replace("%data%", projects.projects[project].description);
         $(".project-entry:last").append(formattedDescription);
 
-// Incase if there are multiple images for a project
+        // Incase if there are multiple images for a project
 
         if (projects.projects[project].images.length > 0) {
             for (image in projects.projects[project].images) {
@@ -162,7 +172,21 @@ projects.display = function () {
     }
 }
 
+
+// Resume sections displayed
+
+bio.display();
+education.display();
+displayWork();
 projects.display();
+
+
+
+
+
+
+
+
 
 
 // Internationalize Names
